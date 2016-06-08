1fc600bf45f863738b39df040f093885887a9afc
Tatiana <alejandra.cespedes@udea.edu.co>
2016-06-07 21:31:09 -0500

    v4

--- app/src/main/java/com/cespedes/tatiana/proyfinal/MainActivity.java
+++ app/src/main/java/com/cespedes/tatiana/proyfinal/MainActivity.java
@@ -14,28 +14,25 @@ import android.widget.TextView;
 import android.support.v4.app.FragmentTransaction;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity{
 
-    matrices[][] ABCDE;
-    matrices[][] FGHIJ;
-    matrices[][] GND;
     ImageView Protoboard;
-    matrices[][] VCC;
+    TextView textView;
+
+    matrices[][] ABCDE, FGHIJ, GND, VCC;
+    matrices lugar;
+
     Bitmap bitmap;
     Canvas canvas;
-    float downx = 0;
-    float downy = 0;
-    float height;
-    matrices lugar;
-    double minx = 1200;
-    double miny = 1200;
     Paint paint;
     StringBuilder stringBuilder = new StringBuilder();
-    TextView textView;
-    float upx = 0;
-    float upy = 0;
-    float width;
+
+    float downx = 0, downy = 0;
+    float upx = 0, upy = 0;
+    double minx = 1200, miny = 1200;
+
 
     protected void onCreate(Bundle savedInstanceState)
     {
@@ -62,6 +59,7 @@ public class MainActivity extends AppCompatActivity{
 
         final CustomHorizontalScrollView SVhorizontal = (CustomHorizontalScrollView) findViewById(R.id.SVhorizontal);
         final CustomScrollView SVvertical = (CustomScrollView) findViewById(R.id.SVvertical);
+
 
 
 
@@ -98,7 +96,7 @@ public class MainActivity extends AppCompatActivity{
 
         Protoboard.setOnTouchListener(new View.OnTouchListener() {
             Path path = new Path();
-            long tiempo, tiempo2;
+            long tiempo, tiempo2, cont=0;
             @Override
             public boolean onTouch(View arg0, MotionEvent arg1) {
                 stringBuilder.setLength(0);
@@ -107,15 +105,18 @@ public class MainActivity extends AppCompatActivity{
                     tiempo = arg1.getEventTime();
                     downx = arg1.getX();
                     downy = arg1.getY();
-                    path.moveTo(downx,downy);
+                    path.moveTo(downx, downy);
                     distancia_minima(downx, downy);
                     //canvas.drawPoint((float)minx, (float)miny, paint);
                 }
 
                 if (arg1.getAction() == MotionEvent.ACTION_MOVE) {
                     tiempo2 = (arg1.getEventTime() - tiempo);
-                    if (tiempo2 > 1000)
-                    {
+                    if (tiempo2 > 500) {
+                        if(cont==0) {
+                            Toast.makeText(getApplicationContext(), "Cablear", Toast.LENGTH_SHORT).show();
+                            cont++;
+                        }
                         upx = arg1.getX();
                         upy = arg1.getY();
                         path.lineTo(upx, upy);
@@ -132,11 +133,14 @@ public class MainActivity extends AppCompatActivity{
                 if(arg1.getAction() == MotionEvent.ACTION_UP){
                     SVhorizontal.setEnableScrolling(true);
                     SVvertical.setEnableScrolling(true);
-                    if(tiempo2> 1000){
+                    cont=0;
+                    if(tiempo2> 500){
                         canvas.drawLine((float)minx, (float)miny, downx, downy, paint);
                         distancia_minima(upx, upy);
-                        canvas.drawLine((float)minx, (float)miny, upx, upy, paint);
+                        canvas.drawLine((float) minx, (float)miny, upx, upy, paint);
+                        Protoboard.invalidate();  //Refresca el ImageView
                     }
+                    tiempo2=0;
                 }
                 //Se muestra en pantalla las coordenadas
                 textView.setText(stringBuilder.toString());
@@ -186,37 +190,73 @@ public class MainActivity extends AppCompatActivity{
 
     public void distancia_minima(double x1, double y1)
     {
-        double minx1, miny1, miny2;
-        int i = 0, j = 0, k = 0;
-        for (int m = 0; m < 63; m++)
-            for (int n = 0; n < 5; n++)
-            {
+        double minx1, miny1, miny2, miny3=1200, miny4=1200,minx3, minx4=1200;
+        int i = 0, j = 0, k = 0, l1=8;
+        int n1=-1, m, n;
+
+        for (m = 0; m < 63; m++) {
+            for (n = 0; n < 5; n++) {
                 minx1 = Math.abs(ABCDE[m][n].getCoordenada_x() - x1);
                 miny1 = Math.abs(ABCDE[m][n].getCoordenada_y() - y1);
                 miny2 = Math.abs(FGHIJ[m][n].getCoordenada_y() - y1);
-                if (minx1 < minx)
-                {
+                if(m<50 && n<2) {
+                    miny3 = Math.abs(VCC[m][n].getCoordenada_y() - y1);
+                    miny4 = Math.abs(GND[m][n].getCoordenada_y() - y1);
+                    minx3 = Math.abs(VCC[m][n].getCoordenada_x() - x1);
+                    if(minx3 < minx4){
+                        minx4= minx3;
+                      //  Toast.makeText(getApplicationContext(), "m =   " + m1, Toast.LENGTH_SHORT).show();
+                        l1 = m;
+                        //Toast.makeText(getApplicationContext(), "l=" + l1, Toast.LENGTH_SHORT).show();
+                    }
+                }
+                if (minx1 < minx) {
                     minx = minx1;
                     j = m;
                 }
-                if (miny1 < miny)
-                {
+                if (miny1 < miny) {
                     miny = miny1;
                     i = n;
                     k = 1;
                 }
-                if (miny2 < miny){
+                if (miny2 < miny) {
                     miny = miny2;
                     i = n;
-                    k = 0;
+                    k = 2;
+                }
+                if (miny3 < miny){
+                    miny = miny3;
+                    i = n;
+                    k = 3;
+                }
+                if (miny4 < miny){
+                    miny = miny4;
+                    i = n;
+                    k = 4;
                 }
             }
-        minx = ABCDE[j][i].getCoordenada_x();
-        if (k == 1) {
-            miny = ABCDE[j][i].getCoordenada_y();
-        }else{
-            miny = FGHIJ[j][i].getCoordenada_y();
         }
+        switch (k){
+            case 1:
+                minx = ABCDE[j][i].getCoordenada_x();
+                miny = ABCDE[j][i].getCoordenada_y();
+                break;
+            case 2:
+                minx = FGHIJ[j][i].getCoordenada_x();
+                miny = FGHIJ[j][i].getCoordenada_y();
+                break;
+            case 3:
+                miny = VCC[l1][i].getCoordenada_y();
+                minx = VCC[l1][i].getCoordenada_x();
+                break;
+            case 4:
+                miny = GND[l1][i].getCoordenada_y();
+                minx = GND[l1][i].getCoordenada_x();
+                break;
+            default:
+                break;
+        }
+
     }
 
 }
